6362a956558047ff12bcebd797c703fc7687da20
mkit2009 <matejkotrba.skola@gmail.com>
2023-02-19 16:11:54 +0100

    Added Tailwind to project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,2 @@
-<script lang="ts">
-	import { Page } from '$lib/types';
-	import { get } from '$lib/db';
-	import { onMount } from 'svelte';
-
-	export let page: Page;
-
-	onMount(async () => {
-		const data = await get('pages', page.slug);
-		console.log(data);
-	});
-</script>
-
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
