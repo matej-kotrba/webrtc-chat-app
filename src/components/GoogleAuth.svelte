6bc41a36657e342b53750067dd8dccb283f439d7
mkit2009 <matejkotrba.skola@gmail.com>
2023-02-24 20:49:14 +0100

    Log out work

--- src/components/GoogleAuth.svelte
+++ src/components/GoogleAuth.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { signInWithPopup } from 'firebase/auth';
 	import { auth, googleAuthProvider } from '../config/firebase';
+	import { signOut } from 'firebase/auth';
 
 	async function signInWithGoogle() {
 		try {
@@ -9,8 +10,22 @@
 			console.log(err);
 		}
 	}
+
+	async function logOutFunc() {
+		try {
+			await signOut(auth);
+		} catch (err) {
+			console.log(err);
+		}
+	}
 </script>
 
-<button on:click={signInWithGoogle} class="p-2 bg-gray-700 text-md rounded-md hover:bg-slate-500">
-	Sign in with Google
-</button>
+{#if !auth.currentUser}
+	<button on:click={signInWithGoogle} class="p-2 bg-gray-700 text-md rounded-md hover:bg-slate-500">
+		Sign in with Google
+	</button>
+{:else}
+	<button on:click={logOutFunc} class="p-2 bg-gray-700 text-md rounded-md hover:bg-slate-500">
+		Log out
+	</button>
+{/if}
