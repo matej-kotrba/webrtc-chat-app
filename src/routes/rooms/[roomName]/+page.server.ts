8332861d5d8ed09b54697fbd686c2713a11d4a9f
mkit2009 <matejkotrba.skola@gmail.com>
2023-02-26 17:45:53 +0100

    Checking if room exists event on page url access done

--- src/routes/rooms/[roomName]/+page.server.ts
+++ src/routes/rooms/[roomName]/+page.server.ts
@@ -1,9 +1,20 @@
 import type { ServerLoad } from "@sveltejs/kit";
 import { collection, getDocs, query, where } from "firebase/firestore"
 import { firestore } from "../../../config/firebase"
-import { redirect } from "@sveltejs/kit";
+import { error } from "@sveltejs/kit";
 
 export const load: ServerLoad = async ({ params }) => {
+  const room = params.roomName
+
+  const rooms = collection(firestore, "rooms")
+  const q = query(rooms, where("title", "==", room))
+
+  const doc = (await getDocs(q)).docs[0]
+
+  if (!doc) {
+    error(404, "It seems that room doesn't exist 😕")
+  }
+
   return {
     roomName: params.roomName as string,
     roomId: doc.id as string
--- src/routes/rooms/[roomName]/+page.svelte
+++ src/routes/rooms/[roomName]/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
  import type {PageData} from "./$types"
  
- export let data: PageData
+ export let data: PageData;
 </script>
 
-<h2>{data.roomName}</h2>
+<h2><span class="text-white text-2xl font-extralight">Room: </span><span class="text-4xl font-extrabold">{data.roomName}</span></h2>
+<span></span>
