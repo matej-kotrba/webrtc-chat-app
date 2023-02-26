4b2310a630a99da5d78cd4af77f8a9103bd4e503
mkit2009 <matejkotrba.skola@gmail.com>
2023-02-26 19:04:20 +0100

    Tooltip and setup copy to clipboard

--- src/routes/rooms/[roomName]/+page.svelte
+++ src/routes/rooms/[roomName]/+page.svelte
@@ -1,8 +1,20 @@
 <script lang="ts">
- import type {PageData} from "./$types"
- 
- export let data: PageData;
+	import Tooltip from '../../../components/Tooltip.svelte';
+
+	import type { PageData } from './$types';
+
+	export let data: PageData;
 </script>
 
-<h2><span class="text-white text-2xl font-extralight">Room: </span><span class="text-4xl font-extrabold">{data.roomName}</span></h2>
-<span></span>
+<h2 class="group">
+	<span class="text-white text-2xl font-extralight">Room: </span><span
+		class="text-4xl font-extrabold"
+		>{data.roomName}
+	</span>
+	<Tooltip text="Copy to clipboard !">
+		<iconify-icon
+			icon="mdi:clipboard-text"
+			class="text-3xl aspect-square opacity-0 group-hover:opacity-100 duration-150"
+		/>
+	</Tooltip>
+</h2>
