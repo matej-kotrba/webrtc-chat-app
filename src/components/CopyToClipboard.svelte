7d4cb62e27567539f28317f95c20c024df09bc4e
mkit2009 <matejkotrba.skola@gmail.com>
2023-02-28 19:18:01 +0100

    Copy to clipboard

---
+++ src/components/CopyToClipboard.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	export let textToCopy: string;
+	export let timeForChange: number = 1000;
+
+	let isChangedToTick = false;
+
+	let changeTimeout: NodeJS.Timeout | null = null;
+
+	function copyToClipboard() {
+		navigator.clipboard.writeText(textToCopy);
+		isChangedToTick = true;
+		if (changeTimeout) clearTimeout(changeTimeout);
+		changeTimeout = setTimeout(() => {
+			isChangedToTick = false;
+			changeTimeout = null;
+		}, timeForChange);
+	}
+</script>
+
+<button
+	on:click={copyToClipboard}
+	class="w-[50xp] aspect-square grid place-items-center"
+>
+	{#if isChangedToTick}
+		<iconify-icon icon="charm:tick" class="text-3xl text-green-500 " />
+	{:else}
+		<iconify-icon
+			icon="mdi:clipboard-text"
+			class="text-3xl aspect-square opacity-100 group-hover:opacity-100 duration-150 "
+		/>
+	{/if}
+</button>
