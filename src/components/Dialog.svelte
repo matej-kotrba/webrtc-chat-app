d3b4006ed3040b3466dd6747c719af5a2376d231
mkit2009 <matejkotrba.skola@gmail.com>
2023-03-05 19:18:02 +0100

    Dialog creation

---
+++ src/components/Dialog.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { fade } from 'svelte/transition';
+
+	let isOpen = false;
+
+	export function open() {
+		isOpen = true;
+	}
+
+	export function close() {
+		isOpen = false;
+	}
+</script>
+
+{#if isOpen}
+	<div
+		transition:fade
+		class="left-[50vw] top-[50vh] translate-x-[-50%] translate-y-[-50%] p-3 rounded-xl"
+	>
+		<button on:click={close} class="ml-auto block"
+			><iconify-icon icon="akar-icons:cross" class="text-lg" /></button
+		>
+		<slot />
+	</div>
+{/if}
