4e746cd185bc535ab36f300544ebeced77bb7d5a
mkit2009 <matejkotrba.skola@gmail.com>
2023-03-11 12:30:11 +0100

    Added error page

---
+++ src/routes/rooms/[roomName]/+error.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<div>
+	<p class="status-code text-white font-extrabold text-center">
+		{$page.status}
+	</p>
+	<p class="text-center text-3xl">
+		{$page.error?.message}
+	</p>
+</div>
+
+<style>
+	.status-code {
+		font-size: 8rem;
+	}
+</style>
