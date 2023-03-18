5a3b5f9c19ecff8d05647342c2760cad16b06d08
mkit2009 <matejkotrba.skola@gmail.com>
2023-03-18 17:31:25 +0100

    Updated look of inputs in BasicForm

--- src/components/forms/BasicForm.svelte
+++ src/components/forms/BasicForm.svelte
@@ -39,11 +39,15 @@
 					? 'p-2 my-2'
 					: ''} flex flex-col justify-center items-center gap-2 relative"
 			>
-				{#if attrs.type !== 'hidden'}
-					<label for={attrs.name}>{text}: </label>
-				{/if}
-				<div class="relative">
-					<input
+				<fieldset
+					class={attrs.type !== 'hidden'
+						? 'border-gray-700 border-2 rounded-lg focus-within:border-indigo-500 text-gray-400 focus-within:text-indigo-400 duration-150'
+						: ''}
+				>
+					{#if attrs.type !== 'hidden'}
+						<legend class="ml-2"><label for={attrs.name}>{text}</label></legend>
+					{/if}
+					<!-- <input
 						{...attrs}
 						class="bg-transparent p-2 text-white outline-none border-solid border-b-2 border-gray-700 peer
 						overflow-hidden text-ellipsis"
@@ -51,8 +55,13 @@
 					<div
 						class="content-[''] absolute w-full h-[2px] bottom-[0px] bg-indigo-600 left-[50%]
 						translate-x-[-50%] scale-x-0 duration-200 peer-focus-within:scale-x-100"
+					/> -->
+					<input
+						type="text"
+						{...attrs}
+						class="bg-transparent p-2 pt-1 pb-3 text-white outline-none overflow-hidden text-ellipsis placeholder:text-gray-500"
 					/>
-				</div>
+				</fieldset>
 			</div>
 			{#if error}
 				<p class="text-red-500 text-center">{error}</p>
