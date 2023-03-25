7f0102b903858e0999a495e46b43a1fab88037e8
mkit2009 <matejkotrba.skola@gmail.com>
2023-03-25 16:53:21 +0100

    Setup of Agora Video Chat functionality

--- src/routes/rooms/[roomId]/+page.svelte
+++ src/routes/rooms/[roomId]/+page.svelte
@@ -1,12 +1,36 @@
 <script lang="ts">
 	import Tooltip from '../../../components/Tooltip.svelte';
 	import CopyToClipboard from '../../../components/CopyToClipboard.svelte';
-
 	import type { PageData } from '../[roomId]/$types';
 	import { browser } from '$app/environment';
 	import VideoCall from '../../../components/VideoCall.svelte';
+	import { PUBLIC_AGORA_APP_ID } from '$env/static/public';
+	import { user } from '../../../stores/user';
+
+	import AgoraRTC from 'agora-rtc-sdk-ng';
 
 	export let data: PageData;
+
+	$: if (browser && $user) {
+		console.log('adsasdasdasd');
+		let token: any = null;
+
+		let client;
+
+		let localTracks = [];
+		let remoteUsers = {};
+
+		async function joinRoomInit() {
+			client = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
+			await client.join(PUBLIC_AGORA_APP_ID, data.roomId, token, $user!.uid);
+
+			joinStream();
+		}
+
+		async function joinStream() {}
+
+		joinRoomInit();
+	}
 </script>
 
 <h2 class="group">
@@ -21,4 +45,4 @@
 	{/if}
 </h2>
 
-<VideoCall roomName={data.roomName} />
+<!-- <VideoCall roomName={data.roomName} /> -->
