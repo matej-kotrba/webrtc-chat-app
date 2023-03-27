efa95c379c183b25139b14514c1d6ca5631e55eb
mkit2009 <matejkotrba.skola@gmail.com>
2023-03-27 20:11:29 +0200

    Update to newest version

--- src/routes/api/userLoginState/+server.ts
+++ src/routes/api/userLoginState/+server.ts
@@ -3,7 +3,12 @@ import type { RequestHandler } from "@sveltejs/kit"
 export const POST: RequestHandler = (async ({ cookies, request }) => {
   const { id } = (await request.json())
   if (!id) {
-    cookies.delete("userId", { path: "/" })
+    cookies.set("userId", "", {
+      path: "/",
+      httpOnly: true,
+      sameSite: "strict",
+    })
+    // cookies.delete("userId", { path: "/" })
     return new Response("Logged out", { status: 200 })
   }
   cookies.set("userId", id, {
