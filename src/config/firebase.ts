c41a362f7eaae169e2808dc7c1d97b5f7e18f271
matej-kotrba <matekotrba.skola@gmail.com>
2023-03-29 23:49:47 +0200

    Fixed the buck with not relogging

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -19,7 +19,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig, String(Math.random()));
+const app = initializeApp(firebaseConfig);
 export const firestore = getFirestore(app);
 export const googleAuthProvider = new GoogleAuthProvider();
 export const auth = getAuth(app);
